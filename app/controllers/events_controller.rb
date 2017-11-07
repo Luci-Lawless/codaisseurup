4132e648b301012c0acad5db1dac490c6dbd52ea
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-07 22:50:23 +0100

    Add view for show event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
   end
 
   def show
-
+    @event = Event.find(params[:id])
   end
 
   def new
@@ -25,7 +25,7 @@ class EventsController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def update
