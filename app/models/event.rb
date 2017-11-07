67d0012b9ef891d70d18c690aa9119e40dfa66fd
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-07 13:50:02 +0100

    Create model Event

---
+++ app/models/event.rb
@@ -0,0 +1,14 @@
+class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+
+  def end_at_after_start_at?
+    if end_at < start_at
+      errors.add :end_at, "Must be after start date"
+    end
+    end_at_after_start_at?
+end
